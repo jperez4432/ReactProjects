732b0deeef5443244390022d67fb6413d6eea104
Juan Perez <jperez4432@gmail.com>
2021-07-12 21:48:16 -0500

    es6 practice

---
+++ ES6 Practice/es6-tutorial.js
@@ -0,0 +1,13 @@
+function sayHello() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log(i);
+}
+sayHello();
+
+const person = {
+    name: 'Juan',
+    walk: function (){},
+    talk() {}
+}
